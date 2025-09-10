0a7841d962b1151182a5b8ae0396f142406c4be6
Quang-Trung-68 <trungdang.dqt@gmail.com>
2025-09-10 22:09:37 +0700

    f8-fullstack-day27 v1.4

--- url-manipulator.js
+++ url-manipulator.js
@@ -6,6 +6,7 @@ const pathname = document.querySelector("#pathname");
 const searchQuery = document.querySelector("#search-query");
 const hash = document.querySelector("#hash");
 const form = document.querySelector(".form");
+const urlPreview = document.querySelector(".url-result");
 
 // form to action
 form.addEventListener("submit", (e) => {
@@ -17,19 +18,27 @@ form.addEventListener("submit", (e) => {
   }
 
   // create standard URL from inputs
-  let urlString =
-    protocol.value + "//" + hostname.value;
+  let urlString = protocol.value + "//" + hostname.value;
 
   if (port.value) urlString += ":" + port.value;
-  if (pathname.value) urlString += pathname.value.startsWith("/") ? pathname.value : "/" + pathname.value;
-  if (searchQuery.value) urlString += searchQuery.value.startsWith("?") ? searchQuery.value : "?" + searchQuery.value;
-  if (hash.value) urlString += hash.value.startsWith("#") ? hash.value : "#" + hash.value;
+  if (pathname.value)
+    urlString += pathname.value.startsWith("/")
+      ? pathname.value
+      : "/" + pathname.value;
+  if (searchQuery.value)
+    urlString += searchQuery.value.startsWith("?")
+      ? searchQuery.value
+      : "?" + searchQuery.value;
+  if (hash.value)
+    urlString += hash.value.startsWith("#") ? hash.value : "#" + hash.value;
 
   const button = e.submitter;
   if (button.value === "navigate") {
     location.assign(urlString);
   } else if (button.value === "replace") {
     location.replace(urlString);
+  } else if (button.value === "preview") {
+    urlPreview.textContent = urlString;
   } else {
     location.reload();
   }
